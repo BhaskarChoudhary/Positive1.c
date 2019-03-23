4df278d22af5019cdc75918844febf3e788599a0
BhaskarChoudhary <40542428+BhaskarChoudhary@users.noreply.github.com>
2019-03-23 22:49:40 +0530

    Create beginner21.c

---
+++ beginner21.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//Bhaskar
+int main()
+{
+	int n,a,d,f;
+	scanf("%d",&n);
+	scanf("%d",&a);
+	scanf("%d",&d);
+	f=(((2*a)+(n-1)*d)*n)/2;
+	//s=(n/2)*((2*a)+(n-1)*d);
+	printf("%d",f);
+	return 0;
+	
+}
