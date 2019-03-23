bb9384670e6607c057ae1991bfb032ed0a0ae94e
BhaskarChoudhary <40542428+BhaskarChoudhary@users.noreply.github.com>
2019-03-23 10:59:32 +0530

    Update and rename beginnerr15.py to beginnerr15.c

---
+++ beginnerr15.c
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class A
+{
+int i;
+public:
+void cal(int n,int q);
+};
+void A::cal(int n,int q)
+{
+for(i=n+1;i<q;i++)
+{
+if(i%2==0)
+std::cout<<i;
+}
+}
+
+int main() {
+int n,q;
+cin>>n;
+cin>>q;
+A a;
+a.cal(n,q);
+return 0;
+}
