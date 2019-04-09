9c7c6191c2e17738c10b80d2aee4c53ddb4448e4
BhaskarChoudhary <40542428+BhaskarChoudhary@users.noreply.github.com>
2019-04-09 16:11:00 +0530

    Create beginner30.c

---
+++ beginner30.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+//bhaskar
+int main()
+{
+ int a,b,c,d,e,f;
+ scanf("%d %d\n",&a,&b);
+ scanf("%d %d",&c,&d);
+ e=abs(a-c);
+ f=abs(b-d);
+ printf("%d %d",e,f);
+ return 0;
+}
