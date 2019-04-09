1c9d70fb206194cd773d60c3a5c9b1a18cd965d6
BhaskarChoudhary <40542428+BhaskarChoudhary@users.noreply.github.com>
2019-04-09 16:03:28 +0530

    Update beginner29.c

--- beginner29.c
+++ beginner29.c
@@ -1,33 +1,10 @@
 #include<stdio.h>
-int len(int n)
-{
-int count = 0;
-while(n != 0)
-    {
-	// n = n/10
-	n /= 10;
-	++count;
-    }
-
-    return count;
-}
 int main()
 {
-int a,b,c,n,s;
-scanf("%d",&n);
-s=len(n);
-if(s>1)
-{
-a=n%10;
-n=n/10;
-b=n%10;
-n=n/10;
-c=n;
-printf("%d %d%d",c,b,a);
-}
-else
-{
-printf("0 %d",n);
-}
-return 0;
+ int a,b,c;
+ scanf("%d",&a);
+ b=a/60;
+ c=a%60;
+ printf("%d %d",b,c);
+ return 0;
 }
