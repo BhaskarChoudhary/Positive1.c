08facd4239ab24ba2c78e40b7af698aaa38bb92a
BhaskarChoudhary <40542428+BhaskarChoudhary@users.noreply.github.com>
2019-04-11 13:26:10 +0530

    Update beginner44.py

--- beginner44.py
+++ beginner44.py
@@ -1,7 +1,7 @@
 a=input()
 count=0
 for i in a:
-    if((i >= chr(58) and i <= chr(64)) or (i >= chr(33) and i <= chr(47))):
+    if((i >= chr(58) and i <= chr(64)) or (i >= chr(33) and i <= chr(47)) or (i >= chr(91) and i <= chr(96))):
         count+=1
     else:
         continue
