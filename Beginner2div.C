56afcb207af86d08e8306cb4403aaa2c27f255cd
BhaskarChoudhary <40542428+BhaskarChoudhary@users.noreply.github.com>
2019-05-14 09:20:52 +0530

    Update Beginner2div.C

--- Beginner2div.C
+++ Beginner2div.C
@@ -1,11 +1,19 @@
 #include<stdio.h>
 int main()
 {
-	int a,i,sum=0;
+	int a,i;
 	scanf("%d",&a);
 	if(a%2==0)
+	{
 	printf("yes");
+	}
+	else if(a==1)
+	{
+		printf("yes");
+	}
 	else
+	{
 	printf("no");
+	}
 	return 0;
 }
