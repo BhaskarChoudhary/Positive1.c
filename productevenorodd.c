6a4ef861d8c2a216d5ce4f7bc8c85ebed1229c57
BhaskarChoudhary <40542428+BhaskarChoudhary@users.noreply.github.com>
2019-05-17 09:08:43 +0530

    Create productevenorodd.c

---
+++ productevenorodd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#bhaskr
+int main() {
+	// your code goes here
+	int n,m,s;
+	scanf("%d %d",&m,&n);
+	s=n*m;
+	if(s%2==0)
+	printf("even");
+	else
+	printf("odd");
+	return 0;
+}
