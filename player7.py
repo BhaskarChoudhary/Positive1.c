a09b3572290ed28ff54b8703be3e38acada5341c
BhaskarChoudhary <40542428+BhaskarChoudhary@users.noreply.github.com>
2019-07-14 02:32:32 +0530

    Create player7.py

---
+++ player7.py
@@ -0,0 +1,23 @@
+a=input()
+l=[]
+d=[]
+p=[]
+for i in a:
+    l.append(ord(i))
+print(l)
+f=len(l)
+i=0
+while(i<f):
+    t=l[i]
+    l[i]=l[i+1]
+    l[i+1]=t
+    d.append(l[i])
+    d.append(l[i+1])
+    i=i+2
+
+for i in d:
+    p.append(chr(i))
+
+res=str("".join(p))
+print(res)
+    
