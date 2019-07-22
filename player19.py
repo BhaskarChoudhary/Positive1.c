784681d4c28afaabd20b84d6b7cea6d24e89fe32
BhaskarChoudhary <40542428+BhaskarChoudhary@users.noreply.github.com>
2019-07-22 22:42:20 +0530

    Create player19.py

---
+++ player19.py
@@ -0,0 +1,18 @@
+a=int(input())
+l=[]
+b=[]
+for i in range(2,a):
+    if(a%i==0):
+        l.append(i)
+for i in l:
+    count=0
+    for j in range(2,i):
+        if(i==2):
+            b.append(2)
+            continue
+        elif(i%j==0):
+            count=count+1
+    if(count==0):
+        b.append(i)
+for i in b:
+    print(i,end=" ")
